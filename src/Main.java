24068a5230df65b4a91fcc312852b8161ef4b223
Joost van Herwaarden <jherwaar2@avans.nl>
2017-12-08 11:38:32 +0100

    Boodschap!

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Roep anders even een boodschap naar binnen.");
+
+        GetBoodschap();
+    }
+
+    public static void GetBoodschap(){
+        System.out.println("Boodschap!");
+    }
+}
