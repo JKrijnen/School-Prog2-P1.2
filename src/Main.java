e0c639211d5b2b6d78c849b186e2d61ea04ae422
Joost van Herwaarden <jherwaar2@avans.nl>
2017-12-18 12:54:51 +0100

    Boodschap!

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
+import java.sql.*;
+
+/**
+ * Dit is een voorbeeld Java toepassing waarin je verbinding maakt met een SQLServer database.
+ */
 public class Main {
+
     public static void main(String[] args) {
+
         System.out.println("Roep anders even een boodschap naar binnen.");
 
         GetBoodschap();
+
+
+
     }
 
     public static void GetBoodschap(){
